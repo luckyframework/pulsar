749b2b5703037f9bfd2b52121d0c5170327ed5f4
Paul Smith <paulcsmith0218@gmail.com>
2020-07-16 16:13:02 -0400

    Add TimedEvent

--- spec/pulsar_spec.cr
+++ spec/pulsar_spec.cr
@@ -3,10 +3,14 @@ require "./spec_helper"
 class Pulsar::TestEvent < Pulsar::Event
 end
 
+class Pulsar::TestTimedEvent < Pulsar::TimedEvent
+end
+
 describe Pulsar do
   describe "#name" do
     it "auto generates the name from the class" do
       Pulsar::TestEvent.new.name.should eq("Pulsar::TestEvent")
+      Pulsar::TestTimedEvent.new.name.should eq("Pulsar::TestTimedEvent")
     end
   end
 
@@ -14,11 +18,29 @@ describe Pulsar do
     called = false
     Pulsar::TestEvent.subscribe do |event|
       called = true
+      event.started_at.should be_a(Time)
       event.should be_a(Pulsar::TestEvent)
     end
 
     Pulsar::TestEvent.new.publish
 
+    called.should be_true
+  end
+
+  it "allows instrumenting an event to record timing information" do
+    called = false
+    Pulsar::TestTimedEvent.subscribe do |event, duration|
+      called = true
+      event.started_at.should be_a(Time)
+      duration.should be_a(Time::Span)
+      event.should be_a(Pulsar::TestTimedEvent)
+    end
+
+    duration = Pulsar::TestTimedEvent.new.instrument do
+      # Nothing
+    end
+
+    duration.should be_a(Time::Span)
     called.should be_true
   end
 end
