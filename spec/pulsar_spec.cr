b98009faa3d32da8c0a2ba731f7ef35e45c935bc
Paul Smith <paulcsmith@users.noreply.github.com>
2020-07-16 15:17:06 -0400

    Add initial commits (#1)

--- spec/pulsar_spec.cr
+++ spec/pulsar_spec.cr
@@ -1,9 +1,24 @@
 require "./spec_helper"
 
-describe Pulsar do
-  # TODO: Write tests
+class Pulsar::TestEvent < Pulsar::Event
+end
 
-  it "works" do
-    false.should eq(true)
+describe Pulsar do
+  describe "#name" do
+    it "auto generates the name from the class" do
+      Pulsar::TestEvent.new.name.should eq("Pulsar::TestEvent")
+    end
+  end
+
+  it "allows subscribing and publishing events" do
+    called = false
+    Pulsar::TestEvent.subscribe do |event|
+      called = true
+      event.should be_a(Pulsar::TestEvent)
+    end
+
+    Pulsar::TestEvent.new.publish
+
+    called.should be_true
   end
 end
---
+++ src/pulsar/event.cr
@@ -0,0 +1,27 @@
+abstract class Pulsar::Event
+  getter :started_at
+
+  macro inherited
+    @started_at : Time = Time.utc
+    @finished_at  : Time?
+    class_property subscribers = [] of self -> Nil
+  end
+
+  def self.subscribe(proc)
+    self.subscribers << proc
+  end
+
+  def self.subscribe(&block : self -> Nil)
+    self.subscribers << block
+  end
+
+  def publish
+    self.class.subscribers.each do |s|
+      s.call(self)
+    end
+  end
+
+  def name
+    self.class.name
+  end
+end
